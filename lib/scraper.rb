ad993976b9d44cd5c855c4273eff184827baa3e9
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-22 17:15:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,12 +22,14 @@ class Scraper
   end 
   
   def get_courses
-    
+    self.get_page.css(".post")
   end 
   
   
   def make_courses
-    
+    self.get_courses.each do |post|
+      course = Course.new 
+      
     
   end 
   
